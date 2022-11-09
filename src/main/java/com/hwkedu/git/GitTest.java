a7a2059d5d93b1cf19c4821f937c9490bfc9723b
wenke <wenke@atguigu.com>
2022-11-09 15:47:16 +0800

    second commit

---
+++ src/main/java/com/hwkedu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.hwkedu.git;
+
+/**
+ * @author 侯文柯
+ * @version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git2");
+
+    }
+}
